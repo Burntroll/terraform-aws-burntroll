f1f47f57130792677e299ed8d077a3f441af3e34
Francisco Silva <fco.silva.dev@gmail.com>
2023-06-12 10:25:12 +0300

    Add ec2 resource

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  backend "remote" {
+    organization = "Burntroll"
+    workspaces {
+      name = "Personal"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0e47283f03fc8c90b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
